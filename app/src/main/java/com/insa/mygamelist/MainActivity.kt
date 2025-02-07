9cf6fedbcd094cac40729f8eaa34ef1312109c97
Zoé Le Barillec <zoe.le-barillec@insa-rennes.fr>
2025-02-07 15:01:35 +0100

    TP 2 terminé

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.insa.mygamelist
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.insa.mygamelist.data.Games
 import com.insa.mygamelist.data.IGDB
 import com.insa.mygamelist.ui.theme.MyGamesListTheme
+import java.time.format.TextStyle
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -31,15 +53,61 @@ class MainActivity : ComponentActivity() {
             MyGamesListTheme {
                 Scaffold(topBar = {
                     TopAppBar(colors = topAppBarColors(
-                        containerColor = Color.Magenta,
+                        containerColor = Color(138, 239, 110, 255),
                         titleContentColor = Color.Black,
                     ), title = { Text("My Games List") })
                 }, modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    Text("À remplir", modifier = Modifier.padding(innerPadding))
+                    LazyColumn (modifier = Modifier.padding(innerPadding)){
+                        items(IGDB.games.size){
+                            index ->
+                                val game =IGDB.games[index]
+                                gameCard(game)
+                        }
+                    }
+                    }
 
                 }
             }
         }
+        //@Composable
+        //fun uploadImage(): String
     }
+    @Composable
+    fun gameCard(jeu : Games){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(15.dp)
+                .height(100.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color(184, 184, 187, 255))
+
+        ) {
+            AsyncImage(model = "https:"+IGDB.covers.find({jeu.cover==it.id})?.url,
+                contentDescription = "image",
+                modifier = Modifier.padding(15.dp))
+            Column(){
+                Text(jeu.name,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    style = androidx.compose.ui.text.TextStyle(textDecoration = TextDecoration.Underline)
+                )
+                var deb = "Genre : "
+                for(elt in IGDB.games[0].genres){
+                    deb += IGDB.genres.find({elt == it.id})?.name + ", "
+                }
+                deb.dropLast(2)
+
+                var mygenres = "Genres : " + IGDB.genres.filter{ it.id in jeu.genres }.joinToString(", ") { it.name }
+                Text(text = mygenres,
+                    fontSize = 20.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis)
+            }
+
+    }
+
+
 }
--- app/src/main/java/com/insa/mygamelist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygamelist/data/IGDB.kt
@@ -4,19 +4,53 @@ import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.insa.mygamelist.R
+import okhttp3.internal.platform.Platform
 
 object IGDB {
 
     lateinit var covers: List<Cover>
+    lateinit var games: List<Games>
+    lateinit var genres: List<Genres>
+    lateinit var platform_logos: List<PlatformLogos>
+    lateinit var platforms: List<Platforms>
 
     fun load(context: Context) {
         val coversFromJson: List<Cover> = Gson().fromJson(
             context.resources.openRawResource(R.raw.covers).bufferedReader(),
             object : TypeToken<List<Cover>>() {}.type
         )
-
         covers = coversFromJson
+
+        val gamesFromJson: List<Games> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<Games>>() {}.type
+        )
+        games = gamesFromJson
+
+        val genresFromJson: List<Genres> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genres>>() {}.type
+        )
+        genres = genresFromJson
+
+        val platformLogosFromJson: List<PlatformLogos> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platform_logos).bufferedReader(),
+            object : TypeToken<List<PlatformLogos>>() {}.type
+        )
+        platform_logos = platformLogosFromJson
+
+        val platformsFromJson: List<Platforms> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platforms).bufferedReader(),
+            object : TypeToken<List<Platforms>>() {}.type
+        )
+        platforms = platformsFromJson
     }
 }
 
 data class Cover(val id: Long, val url: String)
+data class Games(val id: Long, val cover: Long, val first_release_date: Long, val genres: List<Int>,
+                 val name: String, val platforms: List<Int>, val summary: String, val total_rating: Double)
+data class Genres(val id: Int, val name: String)
+data class PlatformLogos(val id: Long, val url: String)
+data class Platforms(val id: Int, val name: String, val platforms_logos: Long)
+
